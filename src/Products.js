1aee29ec9e3b0f1a491d6713a9dbcccf5331a631
Karina Guardado Briseno <karinaguardadobriseno@Karinas-MacBook-Pro-2.local>
2021-06-27 21:14:45 -0700

    fourth commit

---
+++ src/Products.js
@@ -0,0 +1,28 @@
+export const Products  =  [
+  {
+    id: 1,
+    image: './products/cologne.jpg',
+    desc: 'Unisex Cologne',
+    post:'',
+    value: 3,
+  },
+  { id: 2, 
+    image: './products/iwatch.jpg',
+    desc: 'Apple iWatch',
+    value: 10,
+  },
+  {
+    id: 3,
+    image: './products/mug.jpg',
+    desc: 'Unique Mug',
+    post:'',
+    value: 0,
+  },
+  {
+    id: 3,
+    image: './products/wallet.jpg',
+    desc: 'Mens Wallet',
+    post:'',
+    value: 6,
+  }
+]
